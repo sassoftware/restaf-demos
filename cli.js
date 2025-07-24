2075b07cf1984809982af973bf265fb5a81e55e0
Deva <deva.kumar@sas.com>
2025-07-24 12:54:50 -0400

    save

--- cli.js
+++ cli.js
@@ -1,8 +1,11 @@
+#!/usr/bin/env node
 import core  from './src/core.js';
 import {config} from 'dotenv';
 config();
+console.log('Loading MCP ServerJS CLI...');
 if (process.argv[2] === 'https'){
     process.env.HTTPS = true;
 }
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+console.log('Starting core...');
 core();
