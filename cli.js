3a106d12e86b5108ce4f10abe8dbce3fb0dce7e6
Deva <deva.kumar@sas.com>
2025-07-24 13:12:02 -0400

    improved cli.js

--- cli.js
+++ cli.js
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
-import core  from './src/core.js';
-import {config} from 'dotenv';
-config();
-console.log('Loading MCP ServerJS CLI...');
-if (process.argv[2] === 'https'){
+import core from './src/core.js';
+import { config } from 'dotenv';
+import fs from 'fs';
+
+if (fs.existsSync('./.env')) {
+    console.log('Loading environment variables from .env file...');
+    config();
+} else {
+    console.log('No .env file found, Using default environment variables...');
+}
+
+if (process.argv[2] === 'https') {
     process.env.HTTPS = true;
 }
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
-console.log('Starting core...');
 core();
