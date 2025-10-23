0b808c072dee184102036a556498d581950eb771
Deva <deva.kumar@sas.com>
2025-10-23 12:17:40 -0400

    save

--- push2acr/push2acr.sh
+++ push2acr/push2acr.sh
@@ -1,5 +1,6 @@
+cd push2acr
 az acr login --name viyafseditcr
 docker build --no-cache -t mcp-serverjs .
-docker tag mcp-serverjs:latest viyafseditcr.azurecr.io/mcp-serverjs:latest
-docker push viyafseditcr.azurecr.io/mcp-serverjs:latest
+docker tag mcp-serverjs:latest viyafseditcr.azurecr.io/mcp-serverjs:alpha
+docker push viyafseditcr.azurecr.io/mcp-serverjs:alpha
 
