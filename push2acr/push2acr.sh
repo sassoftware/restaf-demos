7abe7b3112a15987c01973ec28f01a8306279ef0
Deva <deva.kumar@sas.com>
2025-10-23 14:01:13 -0400

    fix issue with casing

--- push2acr/push2acr.sh
+++ push2acr/push2acr.sh
@@ -1,6 +1,6 @@
-cd push2acr
+
 az acr login --name viyafseditcr
 docker build --no-cache -t mcp-serverjs .
-docker tag mcp-serverjs:latest viyafseditcr.azurecr.io/mcp-serverjs:alpha
-docker push viyafseditcr.azurecr.io/mcp-serverjs:alpha
+docker tag mcp-serverjs:latest viyafseditcr.azurecr.io/mcp-serverjs:dev
+docker push viyafseditcr.azurecr.io/mcp-serverjs:dev
 
