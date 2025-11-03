87f0ebdf78e493e74b4604c5d8c98c0b807d30bd
Deva <deva.kumar@sas.com>
2025-11-03 14:18:18 -0500

    support for remote

--- src/toolhelpers/_deval.js
+++ src/toolhelpers/_deval.js
@@ -1,4 +1,7 @@
-
+/*
+ * Copyright © 2025, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 async function _deval(_appContext, params) {
   debugger;
