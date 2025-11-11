a628d486d659cfd1492adfac35a6f837b482e15f
Deva <deva.kumar@sas.com>
2025-11-11 08:59:13 -0500

    save with fewer logging

--- src/toolhelpers/_jobSubmit.js
+++ src/toolhelpers/_jobSubmit.js
@@ -19,20 +19,18 @@ async function _jobSubmit(params) {
       }
     });
     let logonPayload = await getLogonPayload(_appContext);
-    console.error('Logon payload: ' + JSON.stringify(logonPayload));
+   
     let msg = await store.logon(logonPayload);
-    console.error('Logon message: ' + msg);
-     debugger;
+   
     type = (type == null) ? 'job' : type.toLowerCase();
     console.error(`Submitting job. Name: ${name} Type: ${type} Scenario: ${JSON.stringify(scenario)}`);
      debugger;
     let r = (type === 'definition' || type === 'def')
       ? await restaflib.jesRun(store, name, scenario)
       : await restaflib.jobRun(store, name, scenario);
-    console.error(`Job run result: ${JSON.stringify(r)}`);
+  
     let response = (query === true) ? {tabled: r.tables} : { tables: r.tables, listing: r.listing, log: r.log} ;
-    
-    console.error(`Job submit response: ${JSON.stringify(response)}`);
+
     return {
       content: [{ type: 'text', text: JSON.stringify(response) }],
       structuredContent: response
